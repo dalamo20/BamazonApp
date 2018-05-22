339de5fb6bf59c1b0498d803062544ee3946a173
Daniel A <dalamo90@gmail.com>
2018-05-21 23:50:46 -0500

    README modified

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # BamazonApp
+
+It's just like the Amazon store but with only 10 listed items!
+
+Open the folder with NODE and SEARCH for one of the products by id.
+PURCHASE any number you like and the computer should prompt you EITHER 
+we have it in stock OR tell you your total!
+
+HAVE FUN!
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,63 +1,71 @@
+/* Captains Log: I got as far as prompting the user for selecting an item and amount but could not think 
+of how I will get the function to check the stock. I commented out 2 functions
+that I planned on working with */
+
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
-  
+
     // Your port; if not 3306
     port: 3306,
-  
+
     // Your username
     user: "root",
-  
+
     // Your password
     password: "root",
     database: "bamazon",
     socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
-  });
-  
-  connection.connect(function(err) {
-    connection.query("SELECT * FROM products", function(err, res) {
+});
+
+connection.connect(function (err) {
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         for (var i = 0; i < res.length; i++) {
-          console.log(res[i].id + " | " + res[i].product + " | " + res[i].price);
+            console.log(res[i].id + " | " + res[i].product + " | " + res[i].price);
         }
         console.log("-----------------------------------");
-        start();
-      });
-  });
+    start();
+    });
+});
 
-  function start() {
+function start() {
+    //create two empty variables to store the results of both questions
+    var idSearch = "";
+    var numBought = "";
+    //then create two separate prompts that will have the results function in them
+    //results function will be defined somewhere else
+
     inquirer.prompt([{
         name: "action",
         message: "Search Product By Id:"
-    }, {
+    }]).then(function (res) {
+        console.log(res);
+        // searchById();
+
+    inquirer.prompt([{
         name: "action",
         message: "How Many Would You Like To Buy?"
-    
-    }]).then(function (res){
-        switch (res.action) {
-            case "Search Product By Id:":
-            searchById();
-            break;
-            case "How Many Would You Like To Buy?":
-            amountBought();
-            default:
-            connection.end();
-            break;
-        }
+    }]).then(function(res) {
+        console.log(res);
     });
-  }
-  function searchById() {
-    connection.query("SELECT id FROM products", function(err, res) {
-        if (err) throw err;
-        for (var i = 0; i < res.length; i++) {
-          console.log(res[i].id + " | " + res[i].product + " | " + res[i].price);
-        }
-        console.log("-----------------------------------");
-        start();
-      });
-  }
+    });
+}
+
+
+
+// function searchById() {
+//     connection.query("SELECT id FROM products", function (err, res) {
+//         if (err) throw err;
+//         for (var i = 0; i < res.length; i++) {
+//             console.log(res[i].id + " | " + res[i].product + " | " + res[i].price);
+//         }
+//         console.log("-----------------------------------");
+//         start();
+//     });
+// }
 
 //   function amountBought() {
 //     connection.query("SELECT * FROM products", function(err, res) {
